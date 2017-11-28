aaa6eb95a8635840c2bccee96a42e341c74dcb75
Matthew Larson <mlarson@usi-dev13-mtka.usicorp.usinternet.com>
2017-11-28 07:09:31 -0600

    adding in html

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+<div>
+  <h3>PHP here:</h3>
+</div>
 <?php
 
   $name = 'Matt Larson';
@@ -8,3 +20,5 @@
   echo "Hello, $name";
 
 ?>
+</body>
+</html>
