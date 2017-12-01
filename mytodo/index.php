dff1d4969c6b05af1cac96b4bbb29ed83b1b4b1b
Matt Larson <matt.a.larson@gmail.com>
2017-12-01 13:51:11 -0600

    built up task class

--- mytodo/index.php
+++ mytodo/index.php
@@ -1,13 +1,45 @@
 <?php
 
 class Task {
-     public function __construct()
-     {
-         //Automatically triggerd on instantiation
-     }
+
+    public $description;
+
+    public $completed = false;
+
+    public function __construct($description)
+    {
+        //Automatically triggerd on instantiation
+        $this->description = $description;
+    }
+
+    public function complete()
+    {
+        return $this->completed = true;
+    }
+
+    public function isComplete()
+    {
+        return $this->completed;
+    }
 }
 
 //any args passed here can be excepted in constructor method in class (__construct())
-$task = new Task();
+// $task = new Task('Make a commit'); - new individual task object is instantiated
+
+//setp array of task 
+
+$tasks = [
+    new Task('Go to the store'),
+    new Task('Finish report migration'),
+    new Task('Eat some yummy food'),
+    new Task('Chillax this weekend'),
+    new Task('Get cat litter'),
+    new Task('Don\'t dump the entire database'),
+    new Task('Don\'t delete any repos')
+];
+
+$tasks[0]->complete();
+
+// die(print_r($stasks));
 
 require('index.view.php');
--- mytodo/index.view.php
+++ mytodo/index.view.php
@@ -5,8 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My To Do</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <ul>
+        <?php foreach ($tasks as $task) : ?>
+            <li>
+                <?php if ($task->completed) : ?>
+                    <strike><h3><?= $task->description; ?></h3></strike>
+                <?php else: ?>
+                    <h3><?= $task->description; ?></h3>
+                <?php endif ?>
+                
+
+            </li>
+        <?php endforeach; ?>
+    </ul>
 </body>
 </html>
