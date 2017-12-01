8578862b70309fe65bfcbcfb60483fa2652b6e04
Matt Larson <matt.a.larson@gmail.com>
2017-12-01 14:32:59 -0600

    refactored connectToDb into functions.php

--- mytodo/functions.php
+++ mytodo/functions.php
@@ -1,3 +1,14 @@
 <?php
 
+function connectToDb()
+{
+    try {
+        //sample PDO string new PDO('DBType:host=YOUR_HOST_HERE;dbname=DB_NAME', 'USERNAME', 'PASSWORD');
+        return new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', 'password' );
+    } catch (PDOException $e) {
+        //getMessage() returns the exception message. Can also write in custom string.
+        die($e->getMessage());
+    }
+}
+
 ?>
--- mytodo/index.php
+++ mytodo/index.php
@@ -1,54 +1,51 @@
 <?php
 
-class Task {
+// class Task {
 
-    public $description;
+//     public $description;
 
-    public $completed = false;
+//     public $completed = false;
 
-    public function __construct($description)
-    {
-        //Automatically triggerd on instantiation
-        $this->description = $description;
-    }
+//     public function __construct($description)
+//     {
+//         //Automatically triggerd on instantiation
+//         $this->description = $description;
+//     }
 
-    public function complete()
-    {
-        return $this->completed = true;
-    }
+//     public function complete()
+//     {
+//         return $this->completed = true;
+//     }
 
-    public function isComplete()
-    {
-        return $this->completed;
-    }
-}
+//     public function isComplete()
+//     {
+//         return $this->completed;
+//     }
+// }
 
-//any args passed here can be excepted in constructor method in class (__construct())
-// $task = new Task('Make a commit'); - new individual task object is instantiated
+// //any args passed here can be excepted in constructor method in class (__construct())
+// // $task = new Task('Make a commit'); - new individual task object is instantiated
 
-//setp array of task 
+// //setp array of task 
 
-$tasks = [
-    new Task('Go to the store'),
-    new Task('Finish report migration'),
-    new Task('Eat some yummy food'),
-    new Task('Chillax this weekend'),
-    new Task('Get cat litter'),
-    new Task('Don\'t dump the entire database'),
-    new Task('Don\'t delete any repos')
-];
+// $tasks = [
+//     new Task('Go to the store'),
+//     new Task('Finish report migration'),
+//     new Task('Eat some yummy food'),
+//     new Task('Chillax this weekend'),
+//     new Task('Get cat litter'),
+//     new Task('Don\'t dump the entire database'),
+//     new Task('Don\'t delete any repos')
+// ];
 
-$tasks[0]->complete();
+// $tasks[0]->complete();
 
 // die(print_r($stasks));
 
-try {
-    //sample PDO string new PDO('DBType:host=YOUR_HOST_HERE;dbname=DB_NAME', 'USERNAME', 'PASSWORD');
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', 'password' );
-} catch (PDOException $e) {
-    //getMessage() returns the exception message. Can also write in custom string.
-    die($e->getMessage());
-}
+require('Task.php');
+require('functions.php');
+
+$pdo = connectToDb();
 
 //Prepare SQL Query
 $statement = $pdo->prepare('SELECT * FROM todos');
@@ -57,7 +54,7 @@ $statement = $pdo->prepare('SELECT * FROM todos');
 $statement->execute();
 
 //fetch DB rows as object
-$rows = $statement->fetchAll(PDO::FETCH_OBJ);
+$rows = $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
 
 var_dump($results[0]->description);
 
