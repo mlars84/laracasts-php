2231546249fa45084244530213760dc9314f7f89
Matt Larson <matt.a.larson@gmail.com>
2017-12-01 14:25:01 -0600

    sample mysql connection with PDO

--- mytodo/index.php
+++ mytodo/index.php
@@ -42,4 +42,24 @@ $tasks[0]->complete();
 
 // die(print_r($stasks));
 
+try {
+    //sample PDO string new PDO('DBType:host=YOUR_HOST_HERE;dbname=DB_NAME', 'USERNAME', 'PASSWORD');
+    $pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', 'password' );
+} catch (PDOException $e) {
+    //getMessage() returns the exception message. Can also write in custom string.
+    die($e->getMessage());
+}
+
+//Prepare SQL Query
+$statement = $pdo->prepare('SELECT * FROM todos');
+
+//Execute Query
+$statement->execute();
+
+//fetch DB rows as object
+$rows = $statement->fetchAll(PDO::FETCH_OBJ);
+
+var_dump($results[0]->description);
+
 require('index.view.php');
+
