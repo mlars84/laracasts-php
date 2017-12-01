bb300b337f995092518f3c1867054f6cca045966
Matt Larson <matt.a.larson@gmail.com>
2017-12-01 15:32:22 -0600

    moved connection deets to config.php

--- mytodo/database/Connection.php
+++ mytodo/database/Connection.php
@@ -2,11 +2,17 @@
 
 class Connection
 {
-    public static function make()
+    public static function make($config)
     {
+
         try {
             //sample PDO string new PDO('DBType:host=YOUR_HOST_HERE;dbname=DB_NAME', 'USERNAME', 'PASSWORD');
-            return new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', 'password' );
+            return new PDO(
+                $config['connection'].';dbname='.$config['name'],
+                $config['username'],
+                $config['password'],
+                $config['options']
+            );
         } catch (PDOException $e) {
             //getMessage() returns the exception message. Can also write in custom string.
             die($e->getMessage());
@@ -16,4 +22,4 @@ class Connection
 
 //calling a static method with Scope Resolution Operator, is a token that allows 
 //access to static, constant, and overridden properties or methods of a class.
-$pdo = Connection::make();
+// $pdo = Connection::make();
